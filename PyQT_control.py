d9750951e5beb8cbf9e003d7ae0b4b6b0238d4d8
Felix Semler <felix-semler@web.de>
2023-05-15 17:43:22 +0200

    Chnaged Microstepping

--- PyQT_control.py
+++ PyQT_control.py
@@ -412,7 +412,7 @@ class Configurator(QtWidgets.QMainWindow, Ui_MainWindow):
     def make_endstops(self):
         # Initiate Engines 
         if self.checkbox_x.isChecked():
-            self.x = Motor_Control(gpio_pins=self.gpio_pins['x'])
+            self.x = Motor_Control(gpio_pins=self.gpio_pins['x'],dx=1/8)
         if self.checkbox_y.isChecked():
             raise Exception('Not Implemented')
         if self.checkbox_z.isChecked():
@@ -741,7 +741,7 @@ class Motor_Control:
     dir = False
     delay = 0.05
     def __init__(self, gpio_pins={'enable':17, 'ms1':27, 'ms2':22, 'ms3':10, 'dir':9, 'step':11} , 
-                 dx=1/16):
+                 dx=1/8):
         """
         gpio_pins : dict of pin name to gpio location
          keys required -> ms1 ms2 ms3 enable step dir
