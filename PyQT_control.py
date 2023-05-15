8e2f6ea5e543495b3f0e768bd46426f66fabeaff
Felix Semler <felix-semler@web.de>
2023-05-15 17:31:26 +0200

    pyqt dropdown flaot to int

--- PyQT_control.py
+++ PyQT_control.py
@@ -560,7 +560,7 @@ class Endstop_Window(QtWidgets.QMainWindow, Ui_Endstop_window):
         return
 
     def set_stepsize(self, index):
-        self.own_step = int(self.combobox_step_size.itemText(index))
+        self.own_step = int(float(self.combobox_step_size.itemText(index)))
         return
 
     @QtCore.pyqtSlot()
