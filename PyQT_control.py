b417b8eeb29b034817e3009e96fe8526351c6d44
Felix Semler <felix-semler@web.de>
2023-05-15 16:35:00 +0200

    Fergot to add window class

--- PyQT_control.py
+++ PyQT_control.py
@@ -405,7 +405,7 @@ class Configurator(QtWidgets.QMainWindow, Ui_MainWindow):
         self.grid = Grid_Handler(motor_x=self.x, motor_y=None, motor_z = None)
 
         # TODO: Make window with motor control to define end stops
-        self.endstop_window = 
+        self.endstop_window = Endstop_Window()
         self.endstop_window.show()
     
     @QtCore.pyqtSlot()
