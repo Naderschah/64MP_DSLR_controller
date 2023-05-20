55910d85547a264ea3fab59c32341c0501b3f360
Felix Semler <felix-semler@web.de>
2023-05-20 13:45:51 +0200

    Removed jpeg in automated imaging

--- PyQT_control.py
+++ PyQT_control.py
@@ -644,7 +644,6 @@ class Configurator(QtWidgets.QMainWindow, Ui_MainWindow):
         """Quick utility to make image so that above less cluttered"""
         request = self.camera.capture_request(wait=True)
         request.save_dng(filename)
-        request.save('main',filename+'.jpeg')
         request.release()
         del request
         time.sleep(1)
