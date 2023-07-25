ddf8cf105ce364784a91f351b33bd7b9c7eb2bb9
Naderschah <felix-semler@web.de>
2023-07-26 00:12:18 +0200

    more debugging for tomorrow

--- find_endstops.py
+++ find_endstops.py
@@ -2,6 +2,7 @@ from PyQT_control import Grid_Handler
 from ULN2003Pi import ULN2003
 import RPi.GPIO as GPIO
 import time
+import sys
 
 motor_dir = [1,1,1]
 gpio_pins = {'x': [19,5,0,11],
@@ -19,17 +20,21 @@ res = input('Press Y to start finding endstops, press N to move each motor 100 s
 
 if res.lower() == 'y':
     # X-Axis
-    # Find minimum
-    while True:
+    # Find minimum I DONT UNDERSTAND WHY THIS GETS STUCK IN AN INFINITE LOOP
+    found_endstop = [[False]]
+    while not found_endstop[0][0]:
         # Move 
-        if not GPIO.input(gpio_pins['Endstops'][0][0]):
-            found_endstop = grid.move_dist([-1000])
-        input('type now ', found_endstop)
+        found_endstop = grid.move_dist([-1000])
         print('left move')
         print(found_endstop) 
         time.sleep(1)
-        if found_endstop[0][0] and found_endstop[0][1]=='min':
-            break
+        if found_endstop[0][0]:
+            print([type(i) for i in found_endstop])
+            print(found_endstop)
+            print(found_endstop[0][1]=='min')
+            sys.exit(0)
+            if found_endstop[0][1]=='min':
+                break
     print('Found X min')
 
     # Find maximum
