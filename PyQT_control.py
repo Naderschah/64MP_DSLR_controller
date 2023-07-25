819f90785caed3c35b9562c4abb60d6ee61d4a4c
Naderschah <felix-semler@web.de>
2023-07-25 23:00:43 +0200

    fixed json loading

--- PyQT_control.py
+++ PyQT_control.py
@@ -1149,8 +1149,8 @@ class Grid_Handler:
             if 'zeropoint' in cont: 
                 # Check if any zeropoint not zero and undo
                 if sum([i<0 for i in cont['zeropoint']]+[i>0 for i in cont['zeropoint']])>0:
-                    if 'gridbounds' in cont: cont['gridbounds'] -= cont['zeropoint']
-                    cont['pos'] -= cont['zeropoint']
+                    if 'gridbounds' in cont: cont['gridbounds'] = [cont['gridbounds'][i] - cont['zeropoint'][i] for i in range(cont['gridbounds'])]
+                    cont['pos'] = [cont['pos'][i] - cont['zeropoint'][i] for i in cont['pos']]
                     self.zeropoint = [0,0,0]
             if 'gridbounds' in cont: self.gridbounds = cont['gridbounds']
             self.pos = cont['pos']
