7c3a4c22ebb9f4e6eb73a386f6a02169c61965bb
Felix <felix-semler@web.de>
2024-07-06 14:25:23 +0200

    Allcommit

--- FinalDataProcessing/CombineImages.jl
+++ FinalDataProcessing/CombineImages.jl
@@ -7,8 +7,9 @@ import .Datastructures
 # For MKR
 include("ScalingFunctions.jl")
 import .ScalingFunctions
+import Images
 
-function CentralAlign(IP::Main.Datastructures.ImagingParameters)
+function CentralAlign(IP::Main.Datastructures.ImagingParameters,offsets)
 
     # Get grid of images
     img_name_grid, img_pos_grid = GetImageGrid(IP)
@@ -17,47 +18,135 @@ function CentralAlign(IP::Main.Datastructures.ImagingParameters)
 
     # Generate final array
     f_array = GenerateFinalArray(IP, img_name_grid)
+    println(size(f_array))
+    
+    mask_array = zeros(size(f_array))
 
     # Midway histogram equalization reference image
-    ref_img = load("$(IP.path)$(img_name_grid[trunc(Int, size(img_name_grid)[1]/2), trunc(Int, size(img_name_grid)[2])])")
-
-
+    ref_img = Images.load("$(IP.path)$(img_name_grid[trunc(Int, size(img_name_grid)[1]/2), trunc(Int, size(img_name_grid)[2])])")
     # Iterate over images
     for i= 1:size(img_name_grid)[1]
         for j= 1:size(img_name_grid)[2]
             println("Loading $(IP.path)$(img_name_grid[i,j])")
 
-            img = load("$(IP.path)$(img_name_grid[i,j])")
+            img = Images.load("$(IP.path)$(img_name_grid[i,j])")
 
             # Adjust histogram
-            img = adjust_histogram([ref_img, img], MidwayEqualization(nbins = 256))[2]
+            img = Images.adjust_histogram([ref_img, img], Images.MidwayEqualization(nbins = 256))[2]
             # Format for combination
-            img = permutedims(channelview(img), (2,3,1))
+            img = permutedims(Images.channelview(img), (2,3,1))
             # Orient correctly
             img = img[end:-1:1, 1:end, 1:end]
-            # Select central portion
-            off = trunc.(Int, IP.overlap .* size(img) ./2)
-            img = img[1+off[1]:end-off[1], 1+off[2]:end-off[2], 1:end]
-        
-            # Compute final coordinates
-            f_x = trunc(Int, img_pos_grid[i,j,1] - (j-1)*offsets[2])# + (i-1)*secondary_offsets[2])
-            f_y = trunc(Int, img_pos_grid[i,j,2] - (i-1)*offsets[1])# + (j-1)*secondary_offsets[1])
-        
-            f_array[f_x+1:f_x+size(img)[1], f_y+1:f_y+size(img)[2], 1:end] .= img[1:end, 1:end, 1:end]
-        
+            # Offset
+            off = trunc.(Int, IP.overlap .* size(img) .*2 )
+            # Crop and combine              ORIGINAL
+            #img = img[1+off[1]:end-off[1], 1+off[2]:end-off[2], 1:end]
+
+            #f_x = trunc(Int, img_pos_grid[i,j,1])# inside used to be j-1 and i -1
+            #f_y = trunc(Int, img_pos_grid[i,j,2])# + (j-1)*secondary_offsets[1])
+
+            #f_array[f_x+1:f_x+size(img)[1], f_y+1:f_y+size(img)[2], 1:end] .+= img[1:end, 1:end, 1:end]
+
+            # TRYING WITH OPACITY
+            f_x = trunc(Int, img_pos_grid[i,j,1]+(j-1)*off[1])+1000
+            f_y = trunc(Int, img_pos_grid[i,j,2]+(i-1)*off[2])+1000
+
+            f_array[f_x+1:f_x+size(img)[1], f_y+1:f_y+size(img)[2], :] .+= img[1:end, 1:end, :]
+            mask_array[f_x+1:f_x+size(img)[1], f_y+1:f_y+size(img)[2], :] .+= 1
         end
     end
-
+    
     # Remove all rows and columns of just black pixels
+    f_array= f_array ./ mask_array
+    replace!(f_array, NaN => 0)
+
     f_array= permutedims(f_array, (3,1,2))
-    f_array = colorview(RGB, f_array)
-    f_array=f_array[[any(row.!=RGB{N0f8}(0,0,0)) for row in eachrow(f_array)], :]
-    f_array = f_array[:,[any(col.!=RGB{N0f8}(0,0,0)) for col in eachcol(f_array)]]
+    f_array = Images.colorview(Images.RGB,f_array)
+
+    #f_array = Images.colorview(Images.RGB, map(Images.N0f8, replace!(f_array./maximum(f_array,dims=3), NaN => 0)))
+    #print(f_array[1,5:10])
+    idx1 = [any(row.!=Images.RGB{Images.N0f8}(0,0,0)) for row in eachrow(f_array)]
+    f_array=f_array[idx1, :]
+    idx2 = [any(col.!=Images.RGB{Images.N0f8}(0,0,0)) for col in eachcol(f_array)]
+    f_array = f_array[:,idx2]
+    print(f_array[1,5:10])
+
+    #mask_array=mask_array[idx1, :, :]
+    #mask_array = mask_array[:,idx2, :]
+
+    #f_array = Images.colorview(Images.RGB, map(Images.N0f8,replace!(Images.channelview(f_array) ./ permutedims(mask_array, (3,1,2)),NaN=>0)))
+    
 
     # Return as JuliaImages array
     return f_array
 end
 
+
+function fnameFocused(y,z,e)
+    # Generates focused filenames based on grid
+    return "Focused_y=$(y)_z=$(z)_e=$(e).png"
+end
+
+function GetIdentifiers(ImagingParams)
+    # Gets filename identifiers from path 
+    files = readdir(ImagingParams.path)
+    f_y_z_exp = [split(i, "_") for i in files]
+    f_y_z = [ endswith(i[end], ".png") ? [i[1],parse(Int, String(split(i[2],"=")[2])), parse(Int, String(split(i[3], "=")[2]))] : ["F", -1, -1] for i in f_y_z_exp]
+    y = unique([i[2] for i in f_y_z])
+    z = unique([i[3] for i in f_y_z])
+    y = sort(y)
+    if -1 in y
+        deleteat!(y, findall(x->x==-1, y))
+    end
+    z = sort(z)
+    if -1 in z
+        deleteat!(z, findall(x->x==-1, z))
+    end
+    return y, z
+end
+
+function GetImageGrid(ImagingParams, fnamefunc=fnameFocused)
+
+    x,y = GetIdentifiers(ImagingParams)
+    x = reverse(x)
+    y = reverse(y)
+    println("x: $(x)")
+    println("y: $(y)")
+    img_name_grid = Array{String}(undef,length(y), length(x))
+    img_pos_grid = Array{Float64}(undef,length(y), length(x), 2)
+    for i in eachindex(x)
+        for j in eachindex(y)
+            img_name_grid[end-j+1,end-i+1]  = fnamefunc(x[i],y[j],ImagingParams.exposure)
+            img_pos_grid[j,i,:] = GeneratePxCoordinates([x[i],y[j]], ImagingParams)
+        end
+    end
+    #return img_name_grid[end:-1:1,end:-1:1], img_pos_grid[end:-1:1,end:-1:1,:]
+    println(img_name_grid)
+    return img_name_grid, img_pos_grid
+end
+
+
+function GeneratePxCoordinates(pos, ImagingParams)
+    #Function to determine the expected position of images
+    return pos .* ImagingParams.steps_per_mm ./ (ImagingParams.px_size/ImagingParams.magnification)
+end
+
+function GenerateFinalArray(ImagingParams, img_name_grid)
+    #=
+    Here we generate the final array of images to be used for the final image
+    =#
+    printstyled("Creating final array", color= :red)
+    im_width, im_height = size(img_name_grid) .+ 1
+    println("im_width : $(im_width)") # TODO
+    println("im_height: $(im_height)")
+    f_height = 10000#.im_height * (1-ImagingParams.overlap) * (im_height+1) # TODO +1 tmp shouldnt be required
+    f_width = 10000#.im_width * (1-ImagingParams.overlap) * (im_width+1) # TODO +1 tmp shouldnt be required
+    f_height = trunc(Int,f_height)+10
+    f_width = trunc(Int,f_width)+10
+    final_array = zeros(f_height,f_width,3)
+    println("final array size $(size(final_array))")
+    return final_array
+end
 
 # Uses predefined positional parameters
 # Can be computed naivly or from the MIST algorithm
--- FinalDataProcessing/MainCombine.jl
+++ FinalDataProcessing/MainCombine.jl
@@ -1,14 +1,16 @@
 include("Datastructures.jl")
-#import .Datastructures
+import .Datastructures
 include("CombineImages.jl")
 import .ImageCombining
-
+using Images
 
 
 offsets = [0,0]
 # Create data struct
-#IP = Datastructures.ImagingParameters("NoIR", 2, 1.55*10^-3, 0.00012397, 0.2, "/SaveSpot/Felt/","/SaveSpot/Felt/combined/", 16, 480, 640,offsets)
-# Do alignment
-f_im = ImageCombining.CentralAlign(IP)
-# Save image
-save("$(IP.save_path)Central_Align.png",f_array)
+for i in range(-250,250)
+    IP = Datastructures.ImagingParameters("NoIR", 2, 1.55*10^-3, 0.00019628179470651555, 0.8+(i/1000), "/SaveSpot/Felt/","/SaveSpot/Felt/combined/$(0.8+(i/1000))", 16, 480, 640,offsets)
+    # Do alignment
+    f_im = ImageCombining.CentralAlign(IP,offsets)
+    # Save image
+    Images.save("$(IP.save_path)Central_Align.png",f_im)
+end
